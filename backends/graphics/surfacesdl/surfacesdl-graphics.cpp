5cc631d21ba598c2927701460398208516657731
M Cameron Johnston <rxcam83@gmail.com>
2021-08-08 20:07:56 -0700

    MusicallyInspired Re-Enabled Aspect Ratio Correction.
    Big Thanks!

--- backends/graphics/surfacesdl/surfacesdl-graphics.cpp
+++ backends/graphics/surfacesdl/surfacesdl-graphics.cpp
@@ -882,7 +882,7 @@ void SurfaceSdlGraphicsManager::setupHardwareSize() {
 	_videoMode.overlayWidth = _videoMode.screenWidth * _videoMode.scaleFactor;
 	_videoMode.overlayHeight = _videoMode.screenHeight * _videoMode.scaleFactor;
 
-	if (_videoMode.screenHeight != 200 && _videoMode.screenHeight != 400)
+	if ((_videoMode.screenWidth >> 4) * 10 != _videoMode.screenHeight)
 		_videoMode.aspectRatioCorrection = false;
 
 	_videoMode.hardwareWidth = _videoMode.screenWidth * _videoMode.scaleFactor;
